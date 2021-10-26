4e1b5839d101966af5f0143909535b4de20d94d3
Saravanabalagi Ramachandran <saravanabalagi@hotmail.com>
2021-10-26 18:57:55 +0100

    Add scale under custom

--- src/images/ImageView.svelte
+++ src/images/ImageView.svelte
@@ -1,11 +1,13 @@
 <script>
 import { Tag } from 'svelma';
+import { slide } from 'svelte/transition';
 
 export let image;
 export let advanced = false;
 
 let widthNatural, heightNatural;
 let scale = 1;
+let custom = false;
 
 const handleImageLoad = (e) => {
     const img = e.target;
@@ -27,16 +29,19 @@ const handleImageLoad = (e) => {
 {#if advanced}
     <div class="advanced m-2">
         <div class="info px-2 m-1">{widthNatural} x {heightNatural}</div>
-        <div class="m-1">
-            Scale: 
-            <input class="scaleInput" type="number" bind:value={scale} step={0.1} max={9.9} min={0.1}>
-            x
-        </div>
         <div class="quickScales">
             {#each [1,2,3,4] as s}
-                <div class="mx-1" on:click={() => scale = s}><Tag type={scale==s?'is-info':'is-none'}>{s}x</Tag></div>
+                <div class="mx-1" on:click={() => { scale = s; custom = false; }}><Tag type={scale==s?'is-info':'is-none'}>{s}x</Tag></div>
             {/each}
+            <div class="mx-1" on:click={() => custom = true}><Tag type={custom?'is-info':'is-none'}>custom</Tag></div>
         </div>
+        {#if custom}
+            <div transition:slide class="m-1">
+                Scale: 
+                <input class="scaleInput" type="number" bind:value={scale} step={0.1} max={9.9} min={0.1}>
+                x
+            </div>
+        {/if}
     </div>
 {/if}
 
@@ -51,6 +56,9 @@ const handleImageLoad = (e) => {
     align-items: center;
 }
 .scaleInput {
+    border-top: none;
+    border-left: none;
+    border-right: none;
     width: 50px;
 }
 .info {
